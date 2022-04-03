dbb04e3d862411df275bf5bc494d0433a7185f43
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-04-03 23:30:00 +0200

    Size and leaves

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - func measures size of binary tree
+ * @tree: pointer to node root of tree to measure size
+ * Return: tree size, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	size += binary_tree_size(tree->left);
+
+
+
+	size += binary_tree_size(tree->right);
+
+	++size;
+
+	return (size);
+}
