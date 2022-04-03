6698b4012df3d3f787b552fc7e5c05c0d0bcd9bf
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-04-03 22:46:05 +0200

    New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: parent node address
+ * @value: value input for new node
+ * Return: new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *to_node;
+
+	to_node = malloc(sizeof(binary_tree_t));
+	if (to_node == NULL)
+		return (NULL);
+	to_node->parent = parent;
+	to_node->n = value;
+	to_node->left = NULL;
+	to_node->right = NULL;
+	return (to_node);
+}
