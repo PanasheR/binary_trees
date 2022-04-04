3778f4e3a0904c8677d51994cf80a679865d8fa5
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-04-04 02:13:38 +0200

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,14 +7,12 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *new = calloc(1, sizeof(binary_tree_t));
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
+	if (new == NULL)
 		return (NULL);
-	node->parent = parent;
-	node->n = value;
-	node->left = NULL;
-	node->right = NULL;
-	return (node);
+
+	new->parent = parent;
+	new->n = value;
+	return (new);
 }
